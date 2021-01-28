46107c7b7cd5d9c433f8a15447d3a21341a58f26
Ben <ben@holidev.net>
2021-01-28 12:06:42 +0100

    auto scale

--- src/parallel_coordinate_plot.py
+++ src/parallel_coordinate_plot.py
@@ -6,22 +6,19 @@ data = csv_reader.readcsv('datasets/cars.csv')
 
 npdata = np.array(data["data"])
 
-fig = plt.figure(figsize=[12.8, 90.6])
-
 def plot_data_point(datapoint, fig):
     plt.plot(range(len(datapoint)), datapoint)
 
-# d = npdata[:200]
+# d = npdata[:20]
 d = npdata
+
+fig = plt.figure(figsize=[12.8, len(d) * 0.9])
 
 for point_index in range(d.shape[0]):
     datapoint = d[point_index, :]
     plot_data_point(datapoint, fig)
 
-
-labels = []
-for col in data['columns']:
-  labels.append(col['name'])
+labels = list(map(lambda c: c['name'], data['columns']))
 
 plt.title("parallel coordinate plot")
 plt.xticks(range(len(labels)), labels=labels)
